43f95ee0dedaad875d473a388a64818a7437fa7f
Ilyas Beisenov <ilyas.beisenov@gmail.com>
2023-05-09 16:31:25 +0600

    add logic on changePassword and changeLogin

--- src/pages/NewData/NameChange/NameChange.jsx
+++ src/pages/NewData/NameChange/NameChange.jsx
@@ -1,16 +1,40 @@
 import S from './NameChange.module.scss';
 import React from 'react';
 import logo from '../../../img/logo.png';
+import { getAuth, updateEmail } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const NameChange = () => {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const handleSave = () => {
+    console.log(email);
+    updateEmail(auth.currentUser, email)
+      .then(() => {
+        console.log('Email изменен на:', email);
+      })
+      .catch((error) => {
+        console.error('Ошибка:', error);
+      });
+    navigate('/');
+  };
+
   return (
     <div className={S.container}>
       <div className={S.formWrapper}>
         <form className={S.form}>
           <img className={S.logo} src={logo} alt="logo" />
           <h3 className={S.subTitle}>Новый логин:</h3>
-          <input className={S.input} placeholder="Логин" type="text" />
-          <button className={S.saveButton} type="button">
+          <input
+            className={S.input}
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            placeholder="Логин"
+            type="text"
+          />
+          <button className={S.saveButton} onClick={handleSave} type="button">
             Сохранить
           </button>
         </form>
--- src/pages/NewData/PasswordChange/PasswordChange.jsx
+++ src/pages/NewData/PasswordChange/PasswordChange.jsx
@@ -1,21 +1,55 @@
 import S from './PasswordChange.module.scss';
 import React from 'react';
 import logo from '../../../img/logo.png';
+import { getAuth, updatePassword } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const PasswordChange = () => {
+  const [password, setPassword] = useState('');
+  const [validatePassword, setValidatePassword] = useState('');
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  const handleSave = () => {
+    console.log(password);
+
+    if (password === validatePassword) {
+      updatePassword(user, password)
+        .then(() => {
+          console.log('пароль изменен');
+        })
+        .catch((error) => {
+          console.error('Ошибка:', error);
+        });
+
+      navigate('/');
+    } else {
+      console.error('Пароли не совпадают');
+      return;
+    }
+  };
+
   return (
     <div className={S.container}>
       <div className={S.formWrapper}>
         <form className={S.form}>
           <img className={S.logo} src={logo} alt="logo" />
           <h3 className={S.subTitle}>Новый пароль:</h3>
-          <input className={S.input} placeholder="Пароль" type="text" />
           <input
             className={S.input}
             placeholder="Новый пароль"
             type="password"
+            onChange={(event) => setPassword(event.target.value)}
           />
-          <button className={S.saveButton} type="button">
+          <input
+            className={S.input}
+            placeholder="Повторите пароль"
+            type="password"
+            onChange={(event) => setValidatePassword(event.target.value)}
+          />
+          <button className={S.saveButton} onClick={handleSave} type="button">
             Сохранить
           </button>
         </form>
