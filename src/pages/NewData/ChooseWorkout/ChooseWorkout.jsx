2d9ed30f3d10120a3148c118bad4372e516196ec
Ilyas Beisenov <ilyas.beisenov@gmail.com>
2023-05-10 15:19:57 +0600

    add ChoseWorkout mapping

--- src/pages/NewData/ChooseWorkout/ChooseWorkout.jsx
+++ src/pages/NewData/ChooseWorkout/ChooseWorkout.jsx
@@ -1,10 +1,9 @@
 import S from './ChooseWorkout.module.scss';
-import { Workout } from './Workout';
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const ChooseWorkout = () => {
-
+function Workouts() {
+  const [workouts, setWorkouts] = useState([]);
   const [isActive, setIsActive] = useState([]);
 
   function handleClick(index) {
@@ -19,25 +18,42 @@ const ChooseWorkout = () => {
     setIsActive(newIndexes);
   }
 
+  useEffect(() => {
+    fetch(
+      'https://fitness-587c7-default-rtdb.europe-west1.firebasedatabase.app/workouts.json'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const workoutsData = Object.values(data); // Преобразование объекта данных в массив значений
+        setWorkouts(workoutsData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
-    <div className={S.container}>
-      <div className={S.wrapper}>
-        <h3 className={S.title}>Выберите тренировку</h3>
-        <div className={S.workoutList}>
-          {Workout.map((item, index) => (
-            <div
-              key={item.id}
-              className={`${S.singleWorkout} ${isActive.includes(index) && S.singleWorkout_active}`}
-              onClick={() => handleClick(index)}
-            >
-              <p className={S.singleWorkout__title}>{item.name}</p>
-              <p className={S.singleWorkout__subTitle}>{item.programm}</p>
-            </div>
-          ))}
+    <div>
+      <div className={S.container}>
+        <div className={S.wrapper}>
+          <h3 className={S.title}>Выберите тренировку</h3>
+          <div className={S.workoutList}>
+            {workouts.map((workout) => (
+              <div
+                key={workout.id}
+                className={`${S.singleWorkout} ${
+                  isActive.includes(workout) && S.singleWorkout_active
+                }`}
+                onClick={() => handleClick(workout)}
+              >
+                <p className={S.singleWorkout__title}>{workout.name}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default ChooseWorkout;
+export default Workouts;
